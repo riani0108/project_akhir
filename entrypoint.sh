6cd4a3822354a8467c9bab11f1581a79f937fc75
Putri Aulia Rahma Lubis <putriauliarahma129@gmail.com>
2026-02-04 09:26:59 +0000

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Fixing Laravel permissions"
-chown -R www-data:www-data /var/www
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache
-
-echo "Starting PHP-FPM"
-exec php-fpm
